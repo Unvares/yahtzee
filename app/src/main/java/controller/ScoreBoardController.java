17c072f826db60408ec55c0411516a00667c44c7
unvares <okmanis8@gmail.com>
2024-02-04 12:59:01 +0100

    chore: create JavaDocs for ScoreBoardController

--- app/src/main/java/controller/ScoreBoardController.java
+++ app/src/main/java/controller/ScoreBoardController.java
@@ -1,19 +1,30 @@
 package controller;
 
 import utils.InputHandler;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import utils.ControllerName;
 import view.ScoreBoardView;
 
+/**
+ * Controller for the ScoreBoard.
+ */
 public class ScoreBoardController extends Controller {
+
+  /**
+   * Constructor for ScoreBoardController.
+   */
   public ScoreBoardController() {
     super(new ScoreBoardView(), "menu");
     setState("menu");
   }
 
+  /**
+   * Gets the new controller based on user input.
+   * 
+   * @param inputHandler The input handler for user input.
+   * @return The new controller.
+   */
   @Override
   protected ControllerName getNewController(InputHandler inputHandler) {
     if (getState().equals("menu")) {
@@ -42,6 +53,11 @@ public class ScoreBoardController extends Controller {
     }
   }
 
+  /**
+   * Gets the list of options for the scoreboard.
+   * 
+   * @return The list of options.
+   */
   private List<String> getOptionsList() {
     List<String> optionsList = new ArrayList<>();
 
