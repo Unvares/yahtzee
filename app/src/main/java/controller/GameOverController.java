70e585f8c01fd3b7b7ae7a34c1c18b8037ef0ebd
unvares <okmanis8@gmail.com>
2024-02-04 12:56:25 +0100

    chore: create JavaDocs for GameOverController

--- app/src/main/java/controller/GameOverController.java
+++ app/src/main/java/controller/GameOverController.java
@@ -10,15 +10,26 @@ import utils.InputHandler;
 import utils.ControllerName;
 import view.GameOverView;
 
+/**
+ * Controller for the game over logic.
+ */
 public class GameOverController extends Controller {
   private AppData appData = AppData.getInstance();
   private GameData gameData = appData.getGameData();
 
+  /**
+   * Constructor for GameOverController.
+   */
   public GameOverController() {
     super(new GameOverView());
-
   }
 
+  /**
+   * Gets the new controller based on user input.
+   * 
+   * @param inputHandler The input handler for user input.
+   * @return The new controller.
+   */
   @Override
   protected ControllerName getNewController(InputHandler inputHandler) {
     inputHandler.getAnyInput("Press enter to return to the main menu: ");
