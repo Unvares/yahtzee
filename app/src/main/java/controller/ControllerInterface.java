6475fbb6ab6e36c28822a69015195807ee88d7fa
unvares <okmanis8@gmail.com>
2024-02-04 12:54:59 +0100

    chore: create JavaDocs for ControllerInterface

--- app/src/main/java/controller/ControllerInterface.java
+++ app/src/main/java/controller/ControllerInterface.java
@@ -2,12 +2,34 @@ package controller;
 
 import view.ViewInterface;
 
+/**
+ * Interface for controllers in the application.
+ */
 public interface ControllerInterface {
+
+  /**
+   * Runs the controller, displays the view and handles input.
+   */
   void run();
 
+  /**
+   * Sets the state of the controller.
+   * 
+   * @param state The state to set.
+   */
   public void setState(String state);
 
+  /**
+   * Gets the state of the controller.
+   * 
+   * @return The state of the controller.
+   */
   public String getState();
 
+  /**
+   * Gets the view associated with the controller.
+   * 
+   * @return The view associated with the controller.
+   */
   public ViewInterface getView();
 }
